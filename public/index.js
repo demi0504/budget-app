2771ce60bd60c8db7573c8260218c72438bbac2e
demi0504 <ripley.demi@gmail.com>
2020-09-14 12:26:33 -0500

    service worker activation and install successful

--- public/index.js
+++ public/index.js
@@ -155,9 +155,11 @@ document.querySelector("#sub-btn").onclick = function() {
 //register service worker
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js").then(reg => {
-      console.log("We found your service worker file!", reg);
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(reg => {
+        console.log("We found your service worker file!", reg);
     });
   });
-}
+};
 
